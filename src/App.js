c78b87ef0677fd6cdac75886f04cd44b6dca7d0f
Dojinex <106768058+Dojinex@users.noreply.github.com>
2024-02-24 10:57:52 +0100

    ALX Project - forecastify

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ForecastifyApp from './Components/ForecastifyApp/forecastifyApp';
+
+function App() {
+  return (
+    <div className="App">
+      <ForecastifyApp/>
+    </div>
+  );
+}
+
+export default App;
